34f93a6fafc8990cec01d26df0b1d8dbd8ba4316
Estefani P <141877727+estefpz@users.noreply.github.com>
2024-04-30 22:21:43 -0300

    Add files via upload

---
+++ desafio.java
@@ -0,0 +1,28 @@
+public class desafio {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        String nome = "axel";
+        int xp = 7700;
+
+        String nivel;
+        if (xp < 1000) {
+            nivel = "ferro";
+        } else if (xp <= 2000) {
+            nivel = "bronze";
+        } else if (xp <= 5000) {
+            nivel = "prata";
+        } else if (xp <= 7000) {
+            nivel = "ouro";
+        } else if (xp <= 8000) {
+            nivel = "platina";
+        } else if (xp <= 9000) {
+            nivel = "ascendente";
+        } else if (xp <= 10000) {
+            nivel = "imortal";
+        } else {
+            nivel = "radiante";
+        }
+
+        System.out.println("O heroi " + nome + " esta no nivel " + nivel);
+    }
+}
